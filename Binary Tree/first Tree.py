f252238d3b74a521f72b96c5b3addf961fb9c990
Tushar Goyal <tushargoyal19tj@gmail.com>
2019-08-30 11:42:28 +0530

    first example to make tree

---
+++ Binary Tree/first Tree.py
@@ -0,0 +1,15 @@
+
+
+#Individual node in Binary tree
+
+class Node:
+    def __init__(self, key):
+        self.val=key
+        self.left = None
+        self.right=None
+
+
+# creating root
+root = Node(1)
+
+print(root)
